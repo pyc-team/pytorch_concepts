89763906a982b5689bbe997bb0c59192f5af5479
pietro <barbiero@tutanota.com>
2024-07-22 18:11:25 +0200

    Re-write causal example using pytorch geometric 📝

--- examples/low-level/causal_concept_embedding_model.py
+++ examples/low-level/causal_concept_embedding_model.py
@@ -1,6 +1,7 @@
 import torch
 from sklearn.metrics import accuracy_score
 import networkx as nx
+import torch_geometric as pyg
 
 from torch_concepts.base import ConceptTensor
 from torch_concepts.data import checkmark
@@ -21,22 +22,18 @@ def main():
     concept_names = ["C1", "C2", "C3", "C4"]
     concepts_train = ConceptTensor.concept(c_train, concept_names=concept_names)
     intervention_indexes = ConceptTensor.concept(torch.ones_like(c_train).bool(), concept_names=concept_names)
+    edge_index = pyg.utils.dense_to_sparse(dag)[0]
 
     # define model
     encoder = torch.nn.Sequential(torch.nn.Linear(n_features, emb_size), torch.nn.LeakyReLU())
     c_encoder = ConceptEncoder(emb_size, n_concepts, 2*emb_size, concept_names=concept_names)
-    c_scorer = torch.nn.Sequential(
-        ConceptScorer(2*emb_size, concept_names=concept_names),
-        torch.nn.Sigmoid()
-    )
-    y_predictors = torch.nn.ModuleList(
-        [torch.nn.Sequential(torch.nn.Linear(emb_size, emb_size),
-                             torch.nn.LeakyReLU(),
-                             torch.nn.Linear(emb_size, 1),
-                             torch.nn.Sigmoid())
-         for _ in range(n_concepts)]
-    )
-    model = torch.nn.Sequential(encoder, c_encoder, c_scorer, y_predictors)
+    c_scorer = torch.nn.Sequential(ConceptScorer(2*emb_size, concept_names=concept_names), torch.nn.Sigmoid())
+    mpnn = pyg.nn.Sequential('x, edge_index', [
+        (pyg.nn.GCNConv(emb_size, emb_size, bias=False), 'x, edge_index -> x'),
+        torch.nn.LeakyReLU(inplace=True),
+    ])
+    y_predictor = torch.nn.Sequential(torch.nn.Linear(n_concepts * emb_size, n_concepts), torch.nn.Sigmoid())
+    model = torch.nn.Sequential(encoder, c_encoder, c_scorer, mpnn, y_predictor)
 
     optimizer = torch.optim.AdamW(model.parameters(), lr=0.01)
     loss_form = torch.nn.BCELoss()
@@ -52,11 +49,8 @@ def main():
         c_intervened = CF.intervene(c_pred, concepts_train, intervention_indexes)
         c_mix = CF.concept_embedding_mixture(c_emb, c_intervened)
         # use the DAG to make each concept dependent on parent concepts only
-        c_masked = torch.einsum('ijk,jl->ilk', c_mix, dag)
-        # predict each concept using its parents only
-        y_pred = torch.zeros_like(y_train)
-        for c in range(n_concepts):
-            y_pred[:, c] = y_predictors[c](c_masked[:, c]).squeeze()
+        c_emb_post = mpnn(c_mix.permute(1, 0, 2), edge_index).permute(1, 0, 2).reshape(n_samples, n_concepts * emb_size)
+        y_pred = y_predictor(c_emb_post)
 
         # compute loss
         concept_loss = loss_form(c_pred, c_train)
@@ -76,20 +70,22 @@ def main():
     print(f"Concept accuracy: {concept_accuracy:.2f}")
 
     # at inference time, we can predict each concept using its parents only
+    # generate concept and task predictions
     emb = encoder(x_train)
     c_emb = c_encoder(emb)
     c_pred = ConceptTensor.concept(c_scorer(c_emb), concept_names)
     c_mix = CF.concept_embedding_mixture(c_emb, c_pred)
-    c_masked = torch.einsum('ijk,jl->ilk', c_mix, dag)
+
+    # use the DAG to make each concept dependent on parent concepts only
     digraph = nx.from_numpy_array(dag.numpy(), create_using=nx.DiGraph)
     y_pred = torch.zeros_like(y_train)
     for node in nx.topological_sort(digraph):
         print(f"Node {node} ({concept_names[node]})")
         print(f"Parents: {list(digraph.predecessors(node))}")
-        y_pred[:, node] = y_predictors[node](c_masked[:, node]).squeeze()
+        c_emb_post = mpnn(c_mix.permute(1, 0, 2), edge_index).permute(1, 0, 2).reshape(n_samples, n_concepts * emb_size)
+        y_pred[:, node] = y_predictor(c_emb_post)[:, node].squeeze()
         c_pred[:, node] = y_pred[:, node].clone()
         c_mix = CF.concept_embedding_mixture(c_emb, c_pred)
-        c_masked = torch.einsum('ijk,jl->ilk', c_mix, dag)
 
     non_zero_y = dag.sum(0) > 0
     task_accuracy = accuracy_score(y_train[:, non_zero_y], y_pred[:, non_zero_y] > 0.5)
