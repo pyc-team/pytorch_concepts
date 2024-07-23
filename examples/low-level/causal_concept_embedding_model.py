Fix causal CEM bug on graph propagation 🐛
- Remove GCN normalization (it destroys graph/do interventions)
- Add independent task predictors
- Add PAG learning
- Find fixed point to get test predictions for general graphs (non-DAG)

examples/low-level/causal_concept_embedding_model.py
@@ -1,11 +1,12 @@
 import torch
+from causallearn.search.PermutationBased.GRaSP import grasp
 from sklearn.metrics import accuracy_score
-import networkx as nx
 import torch_geometric as pyg
 
 from torch_concepts.base import ConceptTensor
 from torch_concepts.data import checkmark
 from torch_concepts.nn import ConceptScorer, ConceptEncoder
+from torch_concepts.utils import prepare_pyg_data
 import torch_concepts.nn.functional as CF
 
 
@@ -13,27 +14,34 @@ def main():
     emb_size = 6
     n_epochs = 500
     n_samples = 1000
+    iterations = 100
 
     # load data
-    x_train, c_train, dag = checkmark(n_samples)
+    x_train, c_train, _ = checkmark(n_samples)
     y_train = c_train.clone()
     n_features = x_train.shape[1]
     n_concepts = c_train.shape[1]
     concept_names = ["C1", "C2", "C3", "C4"]
     concepts_train = ConceptTensor.concept(c_train, concept_names=concept_names)
     intervention_indexes = ConceptTensor.concept(torch.ones_like(c_train).bool(), concept_names=concept_names)
-    edge_index = pyg.utils.dense_to_sparse(dag)[0]
 
     # define model
     encoder = torch.nn.Sequential(torch.nn.Linear(n_features, emb_size), torch.nn.LeakyReLU())
     c_encoder = ConceptEncoder(emb_size, n_concepts, 2*emb_size, concept_names=concept_names)
     c_scorer = torch.nn.Sequential(ConceptScorer(2*emb_size, concept_names=concept_names), torch.nn.Sigmoid())
     mpnn = pyg.nn.Sequential('x, edge_index', [
-        (pyg.nn.GCNConv(emb_size, emb_size, bias=False), 'x, edge_index -> x'),
+        (pyg.nn.GCNConv(emb_size, emb_size, add_self_loops=False, normalize=False), 'x, edge_index -> x'),
         torch.nn.LeakyReLU(inplace=True),
     ])
-    y_predictor = torch.nn.Sequential(torch.nn.Linear(n_concepts * emb_size, n_concepts), torch.nn.Sigmoid())
-    model = torch.nn.Sequential(encoder, c_encoder, c_scorer, mpnn, y_predictor)
+    y_predictors = torch.nn.ModuleList([
+        torch.nn.Sequential(torch.nn.Linear(emb_size, 1), torch.nn.Sigmoid())
+    ] * n_concepts)
+    model = torch.nn.Sequential(encoder, c_encoder, c_scorer, mpnn, y_predictors)
+
+    # learn the causal graph structure (it could be replaced by a known graph or differentiable graph learning)
+    G = grasp(c_train.numpy(), score_func='local_score_BDeu').graph
+    graph = ConceptTensor(torch.abs(torch.tensor(G)), concept_names)
+    print(graph)
 
     optimizer = torch.optim.AdamW(model.parameters(), lr=0.01)
     loss_form = torch.nn.BCELoss()
@@ -48,9 +56,13 @@ def main():
         # concept interventions make training faster and reduce leakage
         c_intervened = CF.intervene(c_pred, concepts_train, intervention_indexes)
         c_mix = CF.concept_embedding_mixture(c_emb, c_intervened)
-        # use the DAG to make each concept dependent on parent concepts only
-        c_emb_post = mpnn(c_mix.permute(1, 0, 2), edge_index).permute(1, 0, 2).reshape(n_samples, n_concepts * emb_size)
-        y_pred = y_predictor(c_emb_post)
+        # use the graph to make each concept dependent on parent concepts only
+        node_features, edge_index, batch = prepare_pyg_data(c_mix, graph)
+        c_emb_post = mpnn(node_features, edge_index).reshape(-1, n_concepts, emb_size)
+        # update task predictions using the updated concept embeddings
+        y_pred = torch.zeros_like(y_train)
+        for node, concept_name in enumerate(concept_names):
+            y_pred[:, node] = y_predictors[node](c_emb_post[:, node]).squeeze()
 
         # compute loss
         concept_loss = loss_form(c_pred, c_train)
@@ -63,33 +75,42 @@ def main():
         if epoch % 100 == 0:
             print(f"Epoch {epoch}: Loss {loss.item():.2f}")
 
-    non_zero_y = dag.sum(0) > 0
+    non_zero_y = graph.sum(0) > 0
     task_accuracy = accuracy_score(y_train[:, non_zero_y], y_pred[:, non_zero_y] > 0.5)
     concept_accuracy = accuracy_score(c_train, c_pred > 0.5)
     print(f"Task accuracy: {task_accuracy:.2f}")
     print(f"Concept accuracy: {concept_accuracy:.2f}")
 
-    # at inference time, we can predict each concept using its parents only
-    # generate concept and task predictions
+    # at inference time, we can intervene on the causal graph by:
+    # removing some edges and replacing concept values with ground truth
+    intervened_idxs = ['C1']
+    intervened_graph = CF.intervene_on_concept_graph(graph, indexes=intervened_idxs)
+    c_pred[:, 0] = c_train[:, 0].clone()
+
+    # at inference time, we can predict each concept using its parents only by propagating concepts through the graph
+    # we first generate initial concept embeddings/predictions
     emb = encoder(x_train)
     c_emb = c_encoder(emb)
-    c_pred = ConceptTensor.concept(c_scorer(c_emb), concept_names)
-    c_mix = CF.concept_embedding_mixture(c_emb, c_pred)
+    y_pred = c_pred = ConceptTensor.concept(c_scorer(c_emb), concept_names)
+    # we then look for the fixed point of the graph convolutional network
+    for epoch in range(iterations):
+        # compute concept embeddings
+        c_mix = CF.concept_embedding_mixture(c_emb, ConceptTensor(y_pred, concept_names))
 
-    # use the DAG to make each concept dependent on parent concepts only
-    digraph = nx.from_numpy_array(dag.numpy(), create_using=nx.DiGraph)
-    y_pred = torch.zeros_like(y_train)
-    for node in nx.topological_sort(digraph):
-        print(f"Node {node} ({concept_names[node]})")
-        print(f"Parents: {list(digraph.predecessors(node))}")
-        c_emb_post = mpnn(c_mix.permute(1, 0, 2), edge_index).permute(1, 0, 2).reshape(n_samples, n_concepts * emb_size)
-        y_pred[:, node] = y_predictor(c_emb_post)[:, node].squeeze()
-        c_pred[:, node] = y_pred[:, node].clone()
-        c_mix = CF.concept_embedding_mixture(c_emb, c_pred)
+        # use the graph to generate new concept embeddings using parent concepts only
+        node_features, edge_index, batch = prepare_pyg_data(c_mix, intervened_graph)
+        c_emb_post = mpnn(node_features, edge_index).reshape(-1, n_concepts, emb_size)
 
-    non_zero_y = dag.sum(0) > 0
+        # update concept predictions using the updated concept embeddings (for non-intervened concepts only)
+        for node, concept_name in enumerate(concept_names):
+            if concept_name not in intervened_idxs:
+                y_pred[:, node] = y_predictors[node](c_emb_post[:, node]).squeeze()
+
+    non_zero_y = intervened_graph.sum(0) > 0
     task_accuracy = accuracy_score(y_train[:, non_zero_y], y_pred[:, non_zero_y] > 0.5)
+    concept_accuracy = accuracy_score(c_train, c_pred > 0.5)
     print(f"Task accuracy: {task_accuracy:.2f}")
+    print(f"Concept accuracy: {concept_accuracy:.2f}")
 
     return
 