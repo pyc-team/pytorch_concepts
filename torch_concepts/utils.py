353919d4db91004f31ddd940a5c0d19b17e63754
Mateo Espinosa Zarlenga <mateoespinosaz2@gmail.com>
2024-12-02 23:10:03 +0000

    Updating utils without pytorch_geometric.

--- torch_concepts/utils.py
+++ torch_concepts/utils.py
@@ -1,11 +1,4 @@
-
-import torch
-
-from torch_geometric.utils import dense_to_sparse
-from typing import Tuple, Dict, Union, List
-
-from torch_concepts.base import ConceptTensor
-
+from typing import Dict, Union, List
 
 def validate_and_generate_concept_names(
     concept_names: Dict[int, Union[int, List[str]]]
@@ -60,48 +53,3 @@ def compute_output_size(concept_names: Dict[int, Union[int, List[str]]]) -> int:
             elif isinstance(value, list):
                 output_size *= len(value)
     return output_size
-
-
-def prepare_pyg_data(
-    tensor: ConceptTensor,
-    adjacency_matrix: ConceptTensor,
-) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-    """
-    Prepare PyG data from a ConceptTensor and an adjacency matrix.
-
-    Args:
-        tensor: ConceptTensor of shape (batch_size, n_nodes, emb_size).
-        adjacency_matrix: Adjacency matrix of shape (n_nodes, n_nodes).
-
-    Returns:
-        Tuple[torch.Tensor, torch.Tensor, torch.Tensor]: node_features,
-            edge_index, batch
-    """
-    adjacency_matrix = (
-        adjacency_matrix.to_standard_tensor()
-        if isinstance(adjacency_matrix, ConceptTensor) else adjacency_matrix
-    )
-
-    batch_size, n_nodes, emb_size = tensor.size()
-
-    # Convert adjacency matrix to edge_index
-    edge_index, _ = dense_to_sparse(adjacency_matrix)
-
-    # Prepare node_features
-    # Shape (batch_size * n_nodes, emb_size)
-    node_features = tensor.view(-1, emb_size)
-
-    # Create batch tensor
-    # Shape (batch_size * n_nodes)
-    batch = torch.arange(batch_size).repeat_interleave(n_nodes)
-
-    # Calculate offsets
-    # Shape (batch_size, 1)
-    offsets = torch.arange(batch_size).view(-1, 1) * n_nodes
-    # Shape (1, batch_size * num_edges)
-    offsets = offsets.repeat(1, edge_index.size(1)).view(1, -1)
-
-    # Repeat edge_index and add offsets
-    edge_index = edge_index.repeat(1, batch_size) + offsets
-
-    return node_features, edge_index, batch
