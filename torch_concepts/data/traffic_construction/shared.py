6b7176be53f32ecf852d9e5715e35d054474bcfe
Mateo Espinosa Zarlenga <mateoespinosaz2@gmail.com>
2024-12-03 08:59:36 +0000

    Making assets path be installation dependent.

--- torch_concepts/data/traffic_construction/shared.py
+++ torch_concepts/data/traffic_construction/shared.py
@@ -1,6 +1,10 @@
 """
 Shared global variables for this dataset generation.
 """
+import os
 
 # Directory where all the useful sprites are stored
-SPRITES_DIRECTORY = "assets/"
+SPRITES_DIRECTORY = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    "assets/"
+)
