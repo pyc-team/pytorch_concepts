670a72dc7acc4a762ed73cd4433e838505c69057
Gabriele Ciravegna <48250744+gabrieleciravegna@users.noreply.github.com>
2025-03-28 16:17:03 +0100

    Avoided CEBaBDataset import the __init__.py data in order to avoid having the Dataset package in the library

--- torch_concepts/data/__init__.py
+++ torch_concepts/data/__init__.py
@@ -2,7 +2,7 @@ from .celeba import CelebADataset
 from .mnist import ColorMNISTDataset
 from .toy import ToyDataset, CompletenessDataset
 from .traffic import TrafficLights
-from .cebab import CEBaBDataset
+# from .cebab import CEBaBDataset
 
 __all__ = [
     'TrafficLights',
@@ -10,5 +10,5 @@ __all__ = [
     'CompletenessDataset',
     'ColorMNISTDataset',
     'CelebADataset',
-    'CEBaBDataset'
+  #  'CEBaBDataset'
 ]
