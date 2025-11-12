297a55100210779d97d37488c121f99a8d6c32fa
Pietro Barbiero <Pietro.Barbiero@ibm.com>
2025-11-12 11:52:20 +0100

    Replace deprecated pkg_resources with importlib

--- torch_concepts/data/traffic_construction/shared.py
+++ torch_concepts/data/traffic_construction/shared.py
@@ -1,10 +1,7 @@
 """
 Shared global variables for this dataset generation.
 """
-import pkg_resources
+from importlib import resources
 
-# Directory where all the useful sprites are stored
-SPRITES_DIRECTORY = lambda x: pkg_resources.resource_filename(
-    'torch_concepts',
-    f'assets/{x}',
-)
+def SPRITES_DIRECTORY(x: str) -> str:
+    return str(resources.files("torch_concepts") / "assets" / x)
