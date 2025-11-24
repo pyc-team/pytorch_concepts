952dea6c11a10c147c28a8e8bcb001f0324fb838
Giovanni De Felice <giovanni.de.felice@usi.ch>
2025-11-24 12:50:25 +0100

    removing imports of not yet supperted datasets

--- torch_concepts/data/datamodules/__init__.py
+++ torch_concepts/data/datamodules/__init__.py
@@ -1,10 +1,6 @@
 from .bnlearn import BnLearnDataModule
-from .TODO_colormnist import ColorMNISTDataModule
-from .TODO_fashionmnist import FashionMNISTDataModule
 
 __all__: list[str] = [
     "BnLearnDataModule",
-    "ColorMNISTDataModule",
-    "FashionMNISTDataModule",
 ]
 
